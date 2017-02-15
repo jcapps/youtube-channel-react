f2f41f760d0c2b92d56c24ce34fb4e1d5b23dd03
jcapps <jamesmcapps@gmail.com>
2017-02-15 11:07:46 -0600

    Update webpack.config with environment variables

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,37 +1,43 @@
 import webpack from 'webpack';
 import path from 'path';
+import {YOUTUBE_INFO} from './tools/private/youtubeInfo';
+
+const GLOBALS = {
+    'process.env': {
+        'YOUTUBE_KEY': JSON.stringify(YOUTUBE_INFO.API_KEY),
+        'CHANNEL_ID': JSON.stringify(YOUTUBE_INFO.CHANNEL_ID)
+    }
+};
 
 export default {
-  debug: true,
-  devtool: 'inline-source-map',
-  noInfo: false,
-  entry: [
-    'eventsource-polyfill', // necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
-    path.resolve(__dirname, 'src/index')
-  ],
-  target: 'web',
-  output: {
-    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'src')
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  module: {
-    loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /(\.scss)$/, loaders: ['style', 'css', 'sass']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
-  }
+    debug: true,
+    devtool: 'inline-source-map',
+    noInfo: false,
+    entry: [
+        'eventsource-polyfill', // necessary for hot reloading with IE
+        'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
+        path.resolve(__dirname, 'src/index')
+    ],
+    target: 'web',
+    output: {
+        path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'src')
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin(GLOBALS)
+    ],
+    module: {
+        loaders: [
+            {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+            {test: /(\.css)$/, loaders: ['style', 'css']},
+            {test: /(\.scss)$/, loaders: ['style', 'css', 'sass']},
+            {test: /\.(jpe?g|ico|gif|png|svg|eot|woff|woff2|ttf)$/, loader: 'file', options: {name: '[path][name].[hash].[ext]'}}
+        ]
+    }
 };
