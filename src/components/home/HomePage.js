2eca8e2db8a1de09c93b2aedaadf5deff9d50375
jcapps <jamesmcapps@gmail.com>
2017-02-15 10:51:51 -0600

    Add missing export to HomePage.js

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,7 +4,10 @@ class HomePage extends React.Component {
     render() {
         return(
             <div>
+                <h1>My App</h1>
             </div>
         );
     }
 }
+
+export default HomePage;
