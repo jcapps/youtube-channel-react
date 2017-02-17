1c89ce1a97ba762abbfbb32188ffa1fbebf926f1
jcapps <jamesmcapps@gmail.com>
2017-02-17 11:13:02 -0600

    Rename reducer to match filename

--- src/reducers/playlistReducer.js
+++ src/reducers/playlistReducer.js
@@ -1,7 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function allPlaylistsReducer(state = initialState.playlist, action) {
+export default function playlistReducer(state = initialState.playlist, action) {
     switch(action.type) {
         case types.GET_PLAYLIST_SUCCESS:
             return action.playlist;
