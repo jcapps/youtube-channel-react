284f8d7d5517dbc216d31513320647b4411c84c1
jcapps <jamesmcapps@gmail.com>
2017-02-22 20:09:07 -0600

    Add description to VideoPlayer

--- src/components/common/VideoPlayer.js
+++ src/components/common/VideoPlayer.js
@@ -2,11 +2,18 @@ import React, {PropTypes} from 'react';
 
 const VideoPlayer = ({video}) => {
     const videoUrl = "https://www.youtube.com/embed/" + video.id;
+    let videoDescription = video.snippet.description.split("\n");
     return (
         <div className="video-player">
             <iframe width="640" height="360" src={videoUrl}></iframe>
             <h3>{video.snippet.title}</h3>
-            <p>{video.snippet.description}</p>
+            <p>
+                {videoDescription.map(piece => {
+                    return (
+                        <span key={piece}>{piece}<br/></span>
+                    );
+                })}
+            </p>
         </div>
     );
 };
