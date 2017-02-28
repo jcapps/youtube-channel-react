0f2be4a3fe3b92f542458a27b8d2565cff9ece4b
jcapps <jamesmcapps@gmail.com>
2017-02-28 13:41:49 -0600

    Add profile picture to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
 import TitleBar from './TitleBar';
 import NavBar from './navbar/NavBar';
 
-const Header = () => {
-    return (
-        <header>
-            <TitleBar/>
-            <NavBar/>
-        </header>
-    );
+class Header extends React.Component {
+    render() {
+        return (
+            <header>
+                <TitleBar channel={this.props.channel}/>
+                <NavBar/>
+            </header>
+        );
+    }
+}
+
+Header.propTypes = {
+    channel: PropTypes.object.isRequired
 };
 
-export default Header;
+function mapStateToProps(state) {
+    return {
+        channel: state.channelInfo
+    };
+}
+
+export default connect(mapStateToProps)(Header);
--- src/components/common/TitleBar.js
+++ src/components/common/TitleBar.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 
-const TitleBar = () => {
+const TitleBar = ({channel}) => {
+    let profilePicUrl = "";
+    if (channel.snippet) {
+        profilePicUrl = channel.snippet.thumbnails.default.url;
+    }
     return (
         <div id="titlebar">
+            <img id="profile-thumbnail" src={profilePicUrl} alt="Profile Thumbnail" />
             <h3>James Capps' YouTube Channel</h3>
         </div>
     );
+};
+
+TitleBar.propTypes = {
+    channel: PropTypes.object.isRequired
 };
 
 export default TitleBar;
