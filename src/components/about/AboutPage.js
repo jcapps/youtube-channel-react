190c0ec1380d26d5d790856f83cb6c2227976ba9
jcapps <jamesmcapps@gmail.com>
2017-02-28 12:54:14 -0600

    Populate About page

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -6,10 +6,19 @@ import VideoPlayer from '../common/VideoPlayer';
 
 class AboutPage extends React.Component {
     render() {
-        console.log(this.props.channel);
+        let profilePicUrl = this.props.channel.snippet.thumbnails.medium.url;
+        let description = this.props.channel.snippet.description.split("\n");
         return (
-            <div>
+            <div id="about-page">
+                <img id="profile-pic" src={profilePicUrl} alt="Profile Picture" />
                 <h3>About My Channel</h3>
+                <p>
+                    {description.map(piece => {
+                        return (
+                            <span key={piece}>{piece}<br/></span>
+                        );
+                    })}
+                </p>
             </div>
         );
     }
