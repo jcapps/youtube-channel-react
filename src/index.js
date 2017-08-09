5212e9d58790fc662b2645def1812de7ffbbb055
jcapps <jamesmcapps@gmail.com>
2017-08-09 12:58:45 -0500

    Add react-addons-perf library for performance testing

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import Perf from 'react-addons-perf';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import {Route} from 'react-router';
@@ -13,6 +14,10 @@ import App from './components/App';
 import './styles/styles.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
+
+if (process.env.NODE_ENV !== 'production' && typeof window !== 'undefined') {
+    window.Perf = Perf;
+}
 
 const store = configureStore();
 store.dispatch(getChannelInfo());
