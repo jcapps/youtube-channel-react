66dc01ccb7473d143140876bb1cf44fab40de2ea
jcapps <jamesmcapps@gmail.com>
2017-09-08 09:54:28 -0500

    Move TimePeriodFilter to its own component

---
+++ analytics/components/views/TimePeriodFilter.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import $ from 'jquery';
+import Periods from '../../globals/Periods';
+
+class TimePeriodFilter extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            timePeriod: Periods.TWENTY_EIGHT_DAY
+        };
+        this.changeTimePeriod = this.changeTimePeriod.bind(this);
+        this.setDateRange = this.setDateRange.bind(this);
+    }
+    
+    changeTimePeriod(e) {
+        const timePeriod = e.target.value;
+
+        if (timePeriod == Periods.CUSTOM) {
+            $('#select-custom-range').removeClass('hidden');
+        } else {
+            $('#select-custom-range').addClass('hidden');
+            this.props.changeTimePeriod(timePeriod, null);
+        }
+    }
+
+    setDateRange() {
+        const startDate = $('#start-date').val();
+        const endDate = $('#end-date').val();
+        this.props.changeTimePeriod(this.props.timePeriod, {startDate, endDate});
+    }
+
+    render() {
+        return (
+            <div id="time-period-filter">
+                <select className="views-select" value={this.props.timePeriod} onChange={this.changeTimePeriod}>
+                    <option value={Periods.SEVEN_DAY}>Last 7 Days</option>
+                    <option value={Periods.TWENTY_EIGHT_DAY}>Last 28 Days</option>
+                    <option value={Periods.THIRTY_DAY}>Last 30 Days</option>
+                    <option value={Periods.YEAR}>Last 365 Days</option>
+                    <option value={Periods.LIFETIME}>Lifetime</option>
+                    <option value={Periods.CUSTOM}>Custom range...</option>
+                </select>
+                <div id="select-custom-range" className="hidden">
+                    <input id="start-date" type="date" />
+                    <input id="end-date" type="date" />
+                    <button onClick={this.setDateRange}>Go</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+TimePeriodFilter.propTypes = {
+    changeTimePeriod: PropTypes.func.isRequired,
+    timePeriod: PropTypes.string.isRequired
+};
+
+export default TimePeriodFilter;
--- analytics/components/views/ViewsPage.js
+++ analytics/components/views/ViewsPage.js
@@ -3,29 +3,28 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as d3 from 'd3';
-import $ from 'jquery';
 import lineGraph from '../../graphs/lineGraph';
 import Periods from '../../globals/Periods';
 import * as viewsActions from '../../actions/viewsActions';
 import ContentFilter from './ContentFilter';
+import TimePeriodFilter from './TimePeriodFilter';
 
 export class ViewsPage extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            selectType: Periods.TWENTY_EIGHT_DAY,
+            timePeriod: Periods.TWENTY_EIGHT_DAY,
             filters: ''
         };
         this.renderLineGraphD3 = this.renderLineGraphD3.bind(this);
-        this.changeSelectType = this.changeSelectType.bind(this);
-        this.setDateRange = this.setDateRange.bind(this);
+        this.changeTimePeriod = this.changeTimePeriod.bind(this);
         this.addFilter = this.addFilter.bind(this);
     }
 
     componentWillMount() {
-        const selectType = this.state.selectType;
+        const timePeriod = this.state.timePeriod;
         const filters = this.state.filters;
-        this.props.actions.getViews(selectType, null, filters);
+        this.props.actions.getViews(timePeriod, null, filters);
     }
 
     componentDidMount() {
@@ -40,25 +39,10 @@ export class ViewsPage extends React.PureComponent {
         lineGraph(container, viewsInfo, 'day', 'views');
     }
 
-    changeSelectType(e) {
-        const selectType = e.target.value;
-        this.setState({ selectType: selectType });
-
-        if (selectType == Periods.CUSTOM) {
-            $('#select-custom-range').removeClass('hidden');
-        } else {
-            $('#select-custom-range').addClass('hidden');
-            const filters = this.state.filters;
-            this.props.actions.getViews(selectType, null, filters);
-        }
-    }
-
-    setDateRange() {
-        const startDate = $('#start-date').val();
-        const endDate = $('#end-date').val();
-        const selectType = this.state.selectType;
+    changeTimePeriod(timePeriod, startEndDates) {
+        if (timePeriod != this.state.timePeriod) this.setState({timePeriod: timePeriod});
         const filters = this.state.filters;
-        this.props.actions.getViews(selectType, {startDate, endDate}, filters);
+        this.props.actions.getViews(timePeriod, startEndDates, filters);
     }
 
     addFilter(searchResult) {
@@ -67,9 +51,9 @@ export class ViewsPage extends React.PureComponent {
         if (kind == 'youtube#video') newFilter = 'video==' + searchResult.id.videoId + ';';
         if (kind == 'youtube#playlist') newFilter = 'isCurated==1;playlist==' + searchResult.id.playlistId + ';';
 
-        const {selectType, filters} = this.state;
+        const {timePeriod, filters} = this.state;
         this.setState({filters: newFilter});
-        this.props.actions.getViews(selectType, null, newFilter);
+        this.props.actions.getViews(timePeriod, null, newFilter);
     }
 
     render() {
@@ -79,19 +63,10 @@ export class ViewsPage extends React.PureComponent {
         return (
             <div id="views-page">
                 <h2>Views</h2>
-                <select className="views-select" value={this.state.selectType} onChange={this.changeSelectType}>
-                    <option value={Periods.SEVEN_DAY}>Last 7 Days</option>
-                    <option value={Periods.TWENTY_EIGHT_DAY}>Last 28 Days</option>
-                    <option value={Periods.THIRTY_DAY}>Last 30 Days</option>
-                    <option value={Periods.YEAR}>Last 365 Days</option>
-                    <option value={Periods.LIFETIME}>Lifetime</option>
-                    <option value={Periods.CUSTOM}>Custom range...</option>
-                </select>
-                <div id="select-custom-range" className="hidden">
-                    <input id="start-date" type="date" />
-                    <input id="end-date" type="date" />
-                    <button onClick={this.setDateRange}>Go</button>
-                </div>
+                <TimePeriodFilter
+                    changeTimePeriod={this.changeTimePeriod}
+                    timePeriod={this.state.timePeriod}
+                />
                 <ContentFilter addFilter={this.addFilter}/>
                 <div id="views-graph" />
             </div>
