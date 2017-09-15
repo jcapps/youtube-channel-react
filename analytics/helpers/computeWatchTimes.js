6c90ad3aed4443931d6c186b67051f269cf8f731
jcapps <jamesmcapps@gmail.com>
2017-09-15 16:29:28 -0500

    Fix Object.assign issue

--- analytics/helpers/computeWatchTimes.js
+++ analytics/helpers/computeWatchTimes.js
@@ -1,20 +1,27 @@
 const computeWatchTimes = viewsAndAverageDuration => {
     let watchTimes = Object.assign({}, viewsAndAverageDuration);
+    let watchTimeRows = Object.assign([], viewsAndAverageDuration.rows);
+    let watchTimeColumnHeaders = Object.assign([], viewsAndAverageDuration.columnHeaders);
 
-    const columns = watchTimes.columnHeaders.map(item => {
+    const columns = watchTimeColumnHeaders.map(item => {
         return item.name;
     });
     const viewsIndex = columns.indexOf('views');
     const averageViewDurationIndex = columns.indexOf('averageViewDuration');
 
-    let watchTimeRows = Object.assign([], viewsAndAverageDuration.rows);
+    let newWatchTimeRows = [];
     watchTimeRows.forEach(row => {
+        const newRow = Object.assign([], row);
         const minutesWatchedInSeconds = row[viewsIndex] * row[averageViewDurationIndex];
-        row.push(minutesWatchedInSeconds / 60); // Times should be in minutes
+        newRow.push(minutesWatchedInSeconds / 60); // Times should be in minutes
+        newWatchTimeRows.push(newRow);
     });
 
     const watchTimeColumnHeader = {name: 'watchTime', columnType: 'METRIC', dataType: 'DECIMAL'};
-    watchTimes.columnHeaders.push(watchTimeColumnHeader);
+    watchTimeColumnHeaders.push(watchTimeColumnHeader);
+    
+    watchTimes.columnHeaders = watchTimeColumnHeaders;
+    watchTimes.rows = newWatchTimeRows;
     
     return watchTimes;
 };
