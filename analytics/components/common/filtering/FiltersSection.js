a90ce8e8edecd57b04548f7e30b7a7c7be961672
jcapps <jamesmcapps@gmail.com>
2017-09-18 12:55:39 -0500

    Move filters section to its own shared component

---
+++ analytics/components/common/filtering/FiltersSection.js
@@ -0,0 +1,223 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ContentTypes from '../../../globals/ContentTypes';
+import Periods from '../../../globals/Periods';
+import addGraphFilter from '../../../helpers/addGraphFilter';
+import removeGraphFilter from '../../../helpers/removeGraphFilter';
+import filterArrayIncludes from '../../../helpers/filterArrayIncludes';
+import ContentFilter from './ContentFilter';
+import ContentTypeFilter from './ContentTypeFilter';
+import TimePeriodFilter from './TimePeriodFilter';
+
+class FiltersSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = props.state;
+        
+        this.changeContentType = this.changeContentType.bind(this);
+        this.changeTimePeriod = this.changeTimePeriod.bind(this);
+        this.addFilter = this.addFilter.bind(this);
+        this.removeFilter = this.removeFilter.bind(this);
+        this.clearFilters = this.clearFilters.bind(this);
+        this.renderContentTypeFilter = this.renderContentTypeFilter.bind(this);
+        this.renderAddedFilters = this.renderAddedFilters.bind(this);
+        this.renderClearAllFilters = this.renderClearAllFilters.bind(this);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (
+            this.state.filters !== nextState.filters ||
+            this.state.addedFilters !== nextState.addedFilters ||
+            (this.state.timePeriod !== nextState.timePeriod && nextState.timePeriod == Periods.CUSTOM)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    
+    changeContentType(contentType) {
+        let playlistFilter = {key: 'isCurated', value: '1'};
+        let newFiltersArray = Object.assign([], this.state.filters);
+        if (contentType == ContentTypes.PLAYLISTS) {
+            if (!filterArrayIncludes(this.state.filters, playlistFilter)) {
+                newFiltersArray.push(playlistFilter);
+            }
+        } else {
+            contentType = ContentTypes.ALL;
+            if (filterArrayIncludes(this.state.filters, playlistFilter)) {
+                for (let i = 0; i < newFiltersArray.length; i++) {
+                    if (newFiltersArray[i].key == 'isCurated') {
+                        newFiltersArray.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+        this.setState({
+            contentType: contentType,
+            filters: newFiltersArray
+        });
+
+        const {timePeriod, dateRange, addedFilters} = this.state;
+        this.props.onChangeFilters({
+            contentType,
+            timePeriod,
+            dateRange,
+            filters: newFiltersArray,
+            addedFilters
+        });
+    }
+
+    changeTimePeriod(timePeriod, dateRange) {
+        if (timePeriod != this.state.timePeriod && timePeriod == Periods.CUSTOM) {
+            this.setState({timePeriod: timePeriod});
+            return;
+        }
+
+        const {contentType, filters, addedFilters} = this.state;
+        this.props.onChangeFilters({
+            contentType,
+            timePeriod,
+            dateRange,
+            filters,
+            addedFilters
+        });
+    }
+
+    addFilter(searchResult) {
+        const {newFiltersArray, newAddedFiltersArray, newContentType}
+            = addGraphFilter(searchResult, this.state.filters, this.state.addedFilters);
+
+        const {timePeriod, dateRange} = this.state;
+        this.props.onChangeFilters({
+            contentType: newContentType,
+            timePeriod,
+            dateRange,
+            filters: newFiltersArray,
+            addedFilters: newAddedFiltersArray
+        });
+    }
+
+    removeFilter(e) {
+        let element = e.target;
+        while (element.className.indexOf("added-filter") < 0) {
+            element = element.parentNode;
+        }
+        const filterInfo = JSON.parse(element.children[0].value);
+
+        const {newFiltersArray, newAddedFiltersArray, shouldClearContentTypeFilter}
+            = removeGraphFilter(filterInfo, this.state.filters, this.state.addedFilters);
+        
+        let contentType = this.state.contentType;
+        if (shouldClearContentTypeFilter) {
+            contentType = ContentTypes.ALL;
+        }
+
+        const {timePeriod, dateRange} = this.state;
+        this.props.onChangeFilters({
+            contentType,
+            timePeriod,
+            dateRange,
+            filters: newFiltersArray,
+            addedFilters: newAddedFiltersArray
+        });
+    }
+
+    clearFilters() {
+        let addedFiltersArray = Object.assign([], this.state.addedFilters);
+        let filtersArray = [];
+        let shouldResetContentTypeFilter = false;
+
+        while (addedFiltersArray.length > 0) {
+            const {newFiltersArray, newAddedFiltersArray, shouldClearContentTypeFilter}
+                = removeGraphFilter(addedFiltersArray[0], this.state.filters, addedFiltersArray);
+            addedFiltersArray = newAddedFiltersArray;
+            filtersArray = newFiltersArray;
+            shouldResetContentTypeFilter = shouldClearContentTypeFilter;
+        }
+
+        let contentType = this.state.contentType;
+        if (shouldResetContentTypeFilter) {
+            contentType = ContentTypes.ALL;
+        }
+        
+        const {timePeriod, dateRange} = this.state;
+        this.props.onChangeFilters({
+            contentType,
+            timePeriod,
+            dateRange,
+            filters: filtersArray,
+            addedFilters: addedFiltersArray
+        });
+    }
+
+    renderContentTypeFilter() {
+        const contentType = this.state.contentType;
+        const filtersArray = this.state.filters;
+        if (contentType != ContentTypes.ALL) {
+            if (contentType == ContentTypes.CHANNELS)
+                return;
+            for (let i = 0; i < filtersArray.length; i++) {
+                if (filtersArray[i].key == 'video' || filtersArray[i].key == 'playlist')
+                    return;
+            }
+        }
+        return (
+            <ContentTypeFilter
+                changeContentType={this.changeContentType}
+                contentType={this.state.contentType}
+            />
+        );
+    }
+
+    renderAddedFilters() {
+        const filters = this.state.addedFilters;
+        return filters.map((filter, i) => {
+            return (
+                <div key={i} className="added-filter" title={filter.snippet.title}>
+                    <input className="hidden" value={JSON.stringify(filter)} readOnly="readOnly" />
+                    <div className="added-filter-title">
+                        {filter.snippet.title}
+                    </div>
+                    <button className="remove-filter" onClick={this.removeFilter}>×</button>
+                </div>
+            );
+        });
+    }
+
+    renderClearAllFilters() {
+        if (this.state.addedFilters.length > 0) {
+            return <button id="clear-all-filters" onClick={this.clearFilters}>Clear All Filters</button>;
+        }
+        return;
+    }
+
+    render() {
+        return (
+            <div id="filters-section">
+                <div id="filters">
+                    <ContentFilter
+                        addFilter={this.addFilter}
+                        contentType={this.state.contentType}
+                    />
+                    {this.renderContentTypeFilter()}
+                    <TimePeriodFilter
+                        changeTimePeriod={this.changeTimePeriod}
+                        timePeriod={this.state.timePeriod}
+                    />
+                </div>
+                <div id="added-filters">
+                    {this.renderAddedFilters()}
+                    {this.renderClearAllFilters()}
+                </div>
+            </div>
+        );
+    }
+}
+
+FiltersSection.propTypes = {
+    state: PropTypes.object.isRequired,
+    onChangeFilters: PropTypes.func.isRequired
+};
+
+export default FiltersSection;
--- analytics/components/views/ViewsPage.js
+++ analytics/components/views/ViewsPage.js
@@ -5,14 +5,9 @@ import {bindActionCreators} from 'redux';
 import $ from 'jquery';
 import ContentTypes from '../../globals/ContentTypes';
 import Periods from '../../globals/Periods';
-import addGraphFilter from '../../helpers/addGraphFilter';
-import removeGraphFilter from '../../helpers/removeGraphFilter';
-import filterArrayIncludes from '../../helpers/filterArrayIncludes';
 import formatFiltersString from '../../helpers/formatFiltersString';
 import * as viewsActions from '../../actions/viewsActions';
-import ContentFilter from '../common/filtering/ContentFilter';
-import ContentTypeFilter from '../common/filtering/ContentTypeFilter';
-import TimePeriodFilter from '../common/filtering/TimePeriodFilter';
+import FiltersSection from '../common/filtering/FiltersSection';
 import LineGraph from '../common/graphs/LineGraph';
 
 export class ViewsPage extends React.PureComponent {
@@ -21,25 +16,16 @@ export class ViewsPage extends React.PureComponent {
         this.state = {
             contentType: ContentTypes.ALL,
             timePeriod: Periods.TWENTY_EIGHT_DAY,
-            startEndDates: null,
+            dateRange: null,
             filters: [],
             addedFilters: []
         };
-        this.changeContentType = this.changeContentType.bind(this);
-        this.changeTimePeriod = this.changeTimePeriod.bind(this);
-        this.addFilter = this.addFilter.bind(this);
-        this.removeFilter = this.removeFilter.bind(this);
-        this.clearFilters = this.clearFilters.bind(this);
-        this.renderContentTypeFilter = this.renderContentTypeFilter.bind(this);
-        this.renderAddedFilters = this.renderAddedFilters.bind(this);
-        this.renderClearAllFilters = this.renderClearAllFilters.bind(this);
+        this.getData = this.getData.bind(this);
         this.renderLineGraph = this.renderLineGraph.bind(this);
     }
 
     componentWillMount() {
-        const {timePeriod, startEndDates} = this.state;
-        const filters = formatFiltersString(this.state.filters);
-        this.props.actions.getViews(timePeriod, startEndDates, filters);
+        this.getData(this.state);
     }
 
     componentDidMount() {
@@ -67,153 +53,11 @@ export class ViewsPage extends React.PureComponent {
         $('#views-page .loading-spinner').addClass('hidden');
     }
 
-    changeContentType(contentType) {
-        let playlistFilter = {key: 'isCurated', value: '1'};
-        let newFiltersArray = Object.assign([], this.state.filters);
-        if (contentType == ContentTypes.PLAYLISTS) {
-            if (!filterArrayIncludes(this.state.filters, playlistFilter)) {
-                newFiltersArray.push(playlistFilter);
-            }
-        } else {
-            contentType = ContentTypes.ALL;
-            if (filterArrayIncludes(this.state.filters, playlistFilter)) {
-                for (let i = 0; i < newFiltersArray.length; i++) {
-                    if (newFiltersArray[i].key == 'isCurated') {
-                        newFiltersArray.splice(i, 1);
-                        break;
-                    }
-                }
-            }
-        }
-        this.setState({
-            contentType: contentType,
-            filters: newFiltersArray
-        });
+    getData(state) {
+        this.setState({...state});
 
         this.showLoadingSpinner();
-        const {timePeriod, startEndDates} = this.state;
-        this.props.actions.getViews(timePeriod, startEndDates, formatFiltersString(newFiltersArray));
-    }
-
-    changeTimePeriod(timePeriod, startEndDates) {
-        const filters = this.state.filters;
-        if (timePeriod != this.state.timePeriod) {
-            this.setState({
-                timePeriod: timePeriod,
-                startEndDates: startEndDates
-            });
-            if (timePeriod == Periods.CUSTOM) return;
-        } else if (timePeriod == Periods.CUSTOM) {
-            this.setState({startEndDates: startEndDates});
-        }
-
-        this.showLoadingSpinner();
-        this.props.actions.getViews(timePeriod, startEndDates, formatFiltersString(filters));
-    }
-
-    addFilter(searchResult) {
-        const {newFiltersArray, newAddedFiltersArray, newContentType}
-            = addGraphFilter(searchResult, this.state.filters, this.state.addedFilters);
-
-        this.setState({
-            contentType: newContentType,
-            filters: newFiltersArray,
-            addedFilters: newAddedFiltersArray
-        });
-
-        this.showLoadingSpinner();
-        const {timePeriod, startEndDates} = this.state;
-        this.props.actions.getViews(timePeriod, startEndDates, formatFiltersString(newFiltersArray));
-    }
-
-    removeFilter(e) {
-        let element = e.target;
-        while (element.className.indexOf("added-filter") < 0) {
-            element = element.parentNode;
-        }
-        const filterInfo = JSON.parse(element.children[0].value);
-
-        const {newFiltersArray, newAddedFiltersArray, shouldClearContentTypeFilter}
-            = removeGraphFilter(filterInfo, this.state.filters, this.state.addedFilters);
-        
-        this.setState({
-            filters: newFiltersArray,
-            addedFilters: newAddedFiltersArray
-        });
-        if (shouldClearContentTypeFilter) {
-            this.setState({contentType: ContentTypes.ALL});
-        }
-
-        this.showLoadingSpinner();
-        const {timePeriod, startEndDates} = this.state;
-        this.props.actions.getViews(timePeriod, startEndDates, formatFiltersString(newFiltersArray));
-    }
-
-    clearFilters() {
-        let addedFiltersArray = Object.assign([], this.state.addedFilters);
-        let filtersArray = [];
-        let shouldResetContentTypeFilter = false;
-
-        while (addedFiltersArray.length > 0) {
-            const {newFiltersArray, newAddedFiltersArray, shouldClearContentTypeFilter}
-                = removeGraphFilter(addedFiltersArray[0], this.state.filters, addedFiltersArray);
-            addedFiltersArray = newAddedFiltersArray;
-            filtersArray = newFiltersArray;
-            shouldResetContentTypeFilter = shouldClearContentTypeFilter;
-        }
-
-        this.setState({
-            filters: filtersArray,
-            addedFilters: addedFiltersArray
-        });
-        if (shouldResetContentTypeFilter) {
-            this.setState({contentType: ContentTypes.ALL});
-        }
-        
-        this.showLoadingSpinner();
-        const {timePeriod, startEndDates} = this.state;
-        this.props.actions.getViews(timePeriod, startEndDates, formatFiltersString(filtersArray));
-    }
-
-    renderContentTypeFilter() {
-        const contentType = this.state.contentType;
-        const filtersArray = this.state.filters;
-        if (contentType != ContentTypes.ALL) {
-            if (contentType == ContentTypes.CHANNELS)
-                return;
-            for (let i = 0; i < filtersArray.length; i++) {
-                if (filtersArray[i].key == 'video' || filtersArray[i].key == 'playlist')
-                    return;
-            }
-        }
-        return (
-            <ContentTypeFilter
-                changeContentType={this.changeContentType}
-                contentType={this.state.contentType}
-            />
-        );
-    }
-
-    renderAddedFilters() {
-        const filters = this.state.addedFilters;
-        return filters.map((filter, i) => {
-            return (
-                <div key={i} className="added-filter" title={filter.snippet.title}>
-                    <input className="hidden" value={JSON.stringify(filter)} readOnly="readOnly" />
-                    <div className="added-filter-title">
-                        {filter.snippet.title}
-                    </div>
-                    <button className="remove-filter" onClick={this.removeFilter}>×</button>
-                </div>
-            );
-        });
-    }
-
-    renderClearAllFilters() {
-        if (this.state.addedFilters.length > 0) {
-            return <button id="clear-all-filters" onClick={this.clearFilters}>Clear All Filters</button>;
-        }
-        return;
+        this.props.actions.getViews(state.timePeriod, state.dateRange, formatFiltersString(state.filters));
     }
 
     renderLineGraph() {
@@ -252,21 +96,10 @@ export class ViewsPage extends React.PureComponent {
         return (
             <div id="views-page">
                 <h2>Views</h2>
-                <div id="filters">
-                    <ContentFilter
-                        addFilter={this.addFilter}
-                        contentType={this.state.contentType}
-                    />
-                    {this.renderContentTypeFilter()}
-                    <TimePeriodFilter
-                        changeTimePeriod={this.changeTimePeriod}
-                        timePeriod={this.state.timePeriod}
-                    />
-                </div>
-                <div id="added-filters">
-                    {this.renderAddedFilters()}
-                    {this.renderClearAllFilters()}
-                </div>
+                <FiltersSection
+                    state={this.state}
+                    onChangeFilters={this.getData}
+                />
                 <h4>Total Views: {totalViews.toLocaleString()}</h4>
                 <h4>Total Estimated Minutes Watched: {totalEstimatedMinutesWatched.toLocaleString()}</h4>
                 {this.renderLineGraph()}
