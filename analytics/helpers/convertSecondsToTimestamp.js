d2e61c2932e4a50f2ef8f229e62d316bfe0fcf2d
jcapps <jamesmcapps@gmail.com>
2017-10-02 13:19:04 -0500

    Fix 0-padding mistake

--- analytics/helpers/convertSecondsToTimestamp.js
+++ analytics/helpers/convertSecondsToTimestamp.js
@@ -2,11 +2,12 @@ const convertSecondsToTimestamp = totalSeconds => {
     const hours = Math.floor(totalSeconds / 3600);
     let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
     let seconds = (totalSeconds - (minutes * 60) - (hours * 3600));
+    console.log(hours, minutes, seconds);
     
-    if (seconds.toLocaleString().length == 1) seconds = seconds.toLocaleString() + '0';
+    if (seconds.toLocaleString().length == 1) seconds = '0' + seconds.toLocaleString();
     let time = minutes + ':' + seconds;
     if (hours > 0) {
-        if (minutes.toLocaleString().length == 1) minutes = minutes.toLocaleString() + '0';
+        if (minutes.toLocaleString().length == 1) minutes = '0' + minutes.toLocaleString();
         time = hours.toLocaleString() + ':' + minutes + ':' + seconds;
     }
 
