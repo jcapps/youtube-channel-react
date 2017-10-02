e62624dc9db1346f2b3b441f20ef9661817eefcc
jcapps <jamesmcapps@gmail.com>
2017-10-02 13:20:07 -0500

    Remove console.log

--- analytics/helpers/convertSecondsToTimestamp.js
+++ analytics/helpers/convertSecondsToTimestamp.js
@@ -2,7 +2,6 @@ const convertSecondsToTimestamp = totalSeconds => {
     const hours = Math.floor(totalSeconds / 3600);
     let minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
     let seconds = (totalSeconds - (minutes * 60) - (hours * 3600));
-    console.log(hours, minutes, seconds);
     
     if (seconds.toLocaleString().length == 1) seconds = '0' + seconds.toLocaleString();
     let time = minutes + ':' + seconds;
