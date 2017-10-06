a2708104b8689cdd518c285d7207c771372c93e0
jcapps <jamesmcapps@gmail.com>
2017-10-06 09:22:00 -0500

    Correctly dispatch clearActions

--- analytics/actions/clearActions.js
+++ analytics/actions/clearActions.js
@@ -1,9 +1,13 @@
 import * as types from './actionTypes';
 
 export function clearSearchResults() {
-    return { type: types.CLEAR_SEARCH_RESULTS };
+    return function(dispatch) {
+        dispatch({ type: types.CLEAR_SEARCH_RESULTS });
+    };
 }
 
 export function clearReport() {
-    return { type: types.CLEAR_REPORT };
+    return function(dispatch) {
+        dispatch({ type: types.CLEAR_REPORT });
+    }
 }
