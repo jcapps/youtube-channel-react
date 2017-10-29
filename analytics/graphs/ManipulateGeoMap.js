b0f83ca19820a942d5a8e40294dcdd5dceecbae1
jcapps <jamesmcapps@gmail.com>
2017-10-28 19:04:10 -0500

    Handle country-filtering exceptions

--- analytics/graphs/ManipulateGeoMap.js
+++ analytics/graphs/ManipulateGeoMap.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import retrieveCountryInfo from '../helpers/retrieveCountryInfo';
 
 class ManipulateGeoMap {
@@ -269,20 +270,163 @@ class ManipulateGeoMap {
         return topojson;
     }
 
+    // Create topojson for iso that's currently a 'territory' of another country
+    createMissingTopojson(CountryMap, isoTerritory, isoCountry) {
+        let territoryInfo;
+        if (isoTerritory == 'bes') {
+            territoryInfo = {name: {common: 'Caribbean Netherlands', official: 'Bonaire, Sint Eustatius, and Saba'}, iso: 'BES', altSpellings: ['Bonaire, St. Eustatius, and Saba']};
+        } else {
+            territoryInfo = retrieveCountryInfo(isoTerritory);
+        }
+
+        // Add territory's topojson
+        let countryData = JSON.parse(JSON.stringify(CountryMap.prototype[isoCountry + 'Topo']));
+        let geometries = countryData.objects[isoCountry].geometries;
+        geometries = geometries.filter(geometry => {
+            const territoryInfoString = JSON.stringify(territoryInfo);
+            return territoryInfoString.indexOf(geometry.properties.name) >= 0;
+        });
+        geometries.forEach(geometry => {
+            geometry.properties.name = territoryInfo.name.common;
+            geometry.properties.iso = isoTerritory.toUpperCase();
+        });
+
+        if (isoTerritory == 'sjm') { // Add Jan Mayen to Svalbard and Jan Mayen topojson
+            const index = geometries[0].arcs.findIndex(arc => {
+                return arc[0] == 89;
+            });
+            if (index < 0) {
+                geometries[0].arcs.push([[89]]);
+            }
+        }
+
+        countryData.objects[isoCountry].geometries = geometries;
+        CountryMap.prototype[isoTerritory + 'Topo'] = countryData;
+
+        // Remove territory from country's topojson
+        countryData = CountryMap.prototype[isoCountry + 'Topo'];
+        geometries = countryData.objects[isoCountry].geometries;
+        geometries = geometries.filter(geometry => {
+            const territoryInfoString = JSON.stringify(territoryInfo);
+            return territoryInfoString.indexOf(geometry.properties.name) < 0;
+        });
+
+        if (isoCountry == 'nor') { // Remove Jan Mayen from Norway topojson
+            let arcIndex = -1;
+            const arrayIndex = geometries.findIndex(geometry => {
+                arcIndex = geometry.arcs.findIndex(arc => {
+                    return arc[0] == 89;
+                });
+                return arcIndex >= 0;
+            });
+            
+            if (arcIndex >= 0) {
+                geometries[arrayIndex].arcs.splice(arcIndex, 1);
+            }
+        }
+        
+        countryData.objects[isoCountry].geometries = geometries;
+        CountryMap.prototype[isoCountry + 'Topo'] = countryData;
+    }
+
     // Get a country's map and topojson data. Handle exceptions.
     getCountryMap(iso) {
         iso = iso.toLowerCase();
-        if (iso == 'ala') iso = 'ald'; // Adjust for difference in datamaps library
-        if (iso == 'pse') iso = 'psx'; // Adjust for difference in datamaps library
-        if (iso == 'esh') iso = 'sah'; // Adjust for difference in datamaps library
-        if (iso == 'ssd') iso = 'sds'; // Adjust for difference in datamaps library
-        const CountryMap = require(`datamaps/dist/datamaps.${iso}.min.js`);
-        let countryData = Object.assign(CountryMap.prototype[iso + 'Topo']);
+        let CountryMap;
+        let countryData;
+
+        // Exception: Cocos (Keeling) Islands & Christmas Island
+        if (iso == 'cck' || iso == 'cxr') {
+            CountryMap = require(`datamaps/dist/datamaps.ioa.min.js`);
+            const cckTopo = Object.assign({}, CountryMap.prototype.cckTopo);
+            const cxrTopo = Object.assign({}, CountryMap.prototype.cxrTopo);
+            if ($.isEmptyObject(cckTopo)) {
+                this.createMissingTopojson(CountryMap, 'cck', 'ioa');
+            }
+            if ($.isEmptyObject(cxrTopo)) {
+                this.createMissingTopojson(CountryMap, 'cxr', 'ioa');
+            }
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+        }
+
+        // Exception: France
+        // Territories: Guadaloupe, French Guiana, Martinique, Mayotte, and Réunion
+        else if (iso == 'fra' || iso == 'glp' || iso == 'guf' || iso == 'mtq' || iso == 'myt' || iso == 'reu') {
+            CountryMap = require(`datamaps/dist/datamaps.fra.min.js`);
+            const glpTopo = Object.assign({}, CountryMap.prototype.glpTopo);
+            const gufTopo = Object.assign({}, CountryMap.prototype.gufTopo);
+            const mtqTopo = Object.assign({}, CountryMap.prototype.mtqTopo);
+            const mytTopo = Object.assign({}, CountryMap.prototype.mytTopo);
+            const reuTopo = Object.assign({}, CountryMap.prototype.reuTopo);
+            if ($.isEmptyObject(glpTopo)) {
+                this.createMissingTopojson(CountryMap, 'glp', 'fra');
+            }
+            if ($.isEmptyObject(gufTopo)) {
+                this.createMissingTopojson(CountryMap, 'guf', 'fra');
+            }
+            if ($.isEmptyObject(mtqTopo)) {
+                this.createMissingTopojson(CountryMap, 'mtq', 'fra');
+            }
+            if ($.isEmptyObject(mytTopo)) {
+                this.createMissingTopojson(CountryMap, 'myt', 'fra');
+            }
+            if ($.isEmptyObject(reuTopo)) {
+                this.createMissingTopojson(CountryMap, 'reu', 'fra');
+            }
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+        }
+        
+        // Exception: Netherlands
+        // Territories: Bonaire, Sint Eustatius, and Saba (Caribbean Netherlands)
+        else if (iso == 'nld' || iso == 'bes') {
+            CountryMap = require(`datamaps/dist/datamaps.nld.min.js`);
+            const besTopo = Object.assign({}, CountryMap.prototype.besTopo);
+            if ($.isEmptyObject(besTopo)) {
+                this.createMissingTopojson(CountryMap, 'bes', 'nld');
+            }
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+        }
+        
+        // Exception: Norway
+        // Territories: Bouvet Island and Svalbard and Jan Mayen
+        else if (iso == 'nor' || iso == 'bvt' || iso == 'sjm') {
+            CountryMap = require(`datamaps/dist/datamaps.nor.min.js`);
+            const bvtTopo = Object.assign({}, CountryMap.prototype.bvtTopo);
+            const sjmTopo = Object.assign({}, CountryMap.prototype.sjmTopo);
+            if ($.isEmptyObject(bvtTopo)) {
+                this.createMissingTopojson(CountryMap, 'bvt', 'nor');
+            }
+            if ($.isEmptyObject(sjmTopo)) {
+                this.createMissingTopojson(CountryMap, 'sjm', 'nor');
+            }
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+        }
+        
+        // Exception: New Zealand
+        // Territories: Tokelau
+        else if (iso == 'nzl' || iso == 'tkl') {
+            CountryMap = require(`datamaps/dist/datamaps.nzl.min.js`);
+            const tlkTopo = Object.assign({}, CountryMap.prototype.tklTopo);
+            if ($.isEmptyObject(tlkTopo)) {
+                this.createMissingTopojson(CountryMap, 'tkl', 'nzl');
+            }
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+        }
+        
+        // Everything else
+        else {
+            if (iso == 'ala') iso = 'ald'; // Adjust for difference in datamaps library
+            if (iso == 'pse') iso = 'psx'; // Adjust for difference in datamaps library
+            if (iso == 'esh') iso = 'sah'; // Adjust for difference in datamaps library
+            if (iso == 'ssd') iso = 'sds'; // Adjust for difference in datamaps library
+            CountryMap = require(`datamaps/dist/datamaps.${iso}.min.js`);
+            countryData = Object.assign({}, CountryMap.prototype[iso + 'Topo']);
+            if (iso == 'ald') iso = 'ala'; // Adjust for difference in datamaps library
+            if (iso == 'psx') iso = 'pse'; // Adjust for difference in datamaps library
+            if (iso == 'sah') iso = 'esh'; // Adjust for difference in datamaps library
+            if (iso == 'sds') iso = 'ssd'; // Adjust for difference in datamaps library
+        }
         const objectIso = Object.keys(countryData.objects)[0];
-        if (iso == 'ald') iso = 'ala'; // Adjust for difference in datamaps library
-        if (iso == 'psx') iso = 'pse'; // Adjust for difference in datamaps library
-        if (iso == 'sah') iso = 'esh'; // Adjust for difference in datamaps library
-        if (iso == 'sds') iso = 'ssd'; // Adjust for difference in datamaps library
         if (objectIso != iso) {
             Object.defineProperty(
                 countryData.objects,
