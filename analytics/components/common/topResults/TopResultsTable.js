ad7996e13323ff63fbddcc90846ca5aec44e7543
jcapps <jamesmcapps@gmail.com>
2017-11-22 13:55:09 -0600

    Remove unused import

--- analytics/components/common/topResults/TopResultsTable.js
+++ analytics/components/common/topResults/TopResultsTable.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import ContentTypes from '../../../globals/ContentTypes';
 import Metrics from '../../../globals/Metrics';
 import TopResultsRow from './TopResultsRow';
 import * as playlistActions from '../../../actions/playlistActions';
